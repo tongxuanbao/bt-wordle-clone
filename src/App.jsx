d305ba9496a9a0a2ccb0034c4f09fb16a51ec93b
Bao Tong <baotongxuan@gmail.com>
2022-02-01 19:20:00 +1000

    Tested out the relative path

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import Header from "components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1>WORDLE CLONE</h1>;
+};
+
+export default Header;
